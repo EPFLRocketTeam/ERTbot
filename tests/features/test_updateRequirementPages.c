1c3f953d8367c13254dbaa778de75edd287ae38d
Ryan0S <ryan.svoboda14@gmail.com>
2024-11-11 15:51:00 +0100

    Added test for updateRequirementPages

--- tests/features/test_updateRequirementPages.c
+++ tests/features/test_updateRequirementPages.c
@@ -12,6 +12,8 @@
 #define REQ_PAGE_1_2 "<!--2024_C_SE_PR_REQ_01-->\\n# 2024_C_SE_PR_REQ_01: PR declaration of purpose\\n>**Description**: PR shall design a bi-liquid propulsion system that will propel the LV to its target apogee.\\n\\n<!--2024_C_SE_PR_REQ_01-->"
 #define REQ_PAGE_2_2 "<!--2024_C_SE_PR_REQ_02-->\\n# 2024_C_SE_PR_REQ_02: Total impulse\\n>**Description**: The propulsion system shall produce an impulse of [80000][+15000/-25000]Ns.\\n\\n\\n# Verification\\n## Verification 1\\n**Method**: Test\\n**Deadline**: SIR\\n**Status**: :red_circle:Uncompleted\\n<!--2024_C_SE_PR_REQ_02-->"
 
+#define REQ_PAGE_1_3 "<!--2024_C_SE_PR_REQ_01-->\\n# 2024_C_SE_PR_REQ_01: PR declaration of purpose\\n>**Description**: PR shall design a bi-liquid propulsion system that will propel the LV to its target apogee.\\n\\nFoo.\\n\\n<!--2024_C_SE_PR_REQ_01-->"
+#define REQ_PAGE_2_3 "<!--2024_C_SE_PR_REQ_02-->\\n# 2024_C_SE_PR_REQ_02: Total impulse\\n>**Description**: The propulsion system shall produce an impulse of [80000][+15000/-25000]Ns.\\n\\n\\n# Verification\\n## Verification 1\\n**Method**: Test\\n**Deadline**: SIR\\n**Status**: :red_circle:Uncompleted\\n<!--2024_C_SE_PR_REQ_02-->"
 
 START_TEST(test_updateRequirementPages_1) {
 
@@ -178,6 +180,46 @@ START_TEST(test_updateRequirementPages_4) {
 }
 END_TEST
 
+START_TEST(test_updateRequirementPages_5) {
+
+    initializeApiTokenVariables();
+    
+    refreshOAuthToken();
+
+    command cmd;
+    cmd.function = NULL;
+    cmd.argument = "UT_REQ_5";
+
+    pageList* checkPage1 = NULL;
+    checkPage1 = addPageToList(&checkPage1, "1999", NULL, NULL, NULL, NULL, NULL);
+    getPage(&checkPage1);
+    if(strcmp(checkPage1->content, "<!--2024_C_SE_PR_REQ_01-->\\n<!--2024_C_SE_PR_REQ_01-->")!=0){
+        log_message(LOG_ERROR, "%s: %s", checkPage1->title, checkPage1->content);
+        ck_abort_msg("%s was not reset properly at last test run", checkPage1->title);
+    }
+    freePageList(&checkPage1);
+
+    pageList* checkPage2 = NULL;
+    checkPage2 = addPageToList(&checkPage2, "1996", NULL, NULL, NULL, NULL, NULL);
+    getPage(&checkPage2);
+    if(strcmp(checkPage2->content, "<!--2024_C_SE_PR_REQ_02-->\\n<!--2024_C_SE_PR_REQ_02-->")!=0){
+        log_message(LOG_ERROR, "%s: %s", checkPage2->title, checkPage2->content);
+        ck_abort_msg("%s was not reset properly at last test run", checkPage2->title);
+    }
+    freePageList(&checkPage2);
+
+    updateRequirementPage(cmd);
+
+    char* requirementPage1 = fetchAndModifyPageContent("1999", "<!--2024_C_SE_PR_REQ_01-->\\\\n<!--2024_C_SE_PR_REQ_01-->", requirementPage1);
+    char* requirementPage2 = fetchAndModifyPageContent("1996", "<!--2024_C_SE_PR_REQ_02-->\\\\n<!--2024_C_SE_PR_REQ_02-->", requirementPage2);
+
+    ck_assert_str_eq(requirementPage1, REQ_PAGE_1_3);
+    ck_assert_str_eq(requirementPage2, REQ_PAGE_2_3);
+
+    free(requirementPage1);
+    free(requirementPage2);
+}
+END_TEST
 
 // Test suite setup
 Suite *updateRequirementPages_suite(void) {
@@ -194,6 +236,7 @@ Suite *updateRequirementPages_suite(void) {
     tcase_add_test(tc_core, test_updateRequirementPages_2);
     tcase_add_test(tc_core, test_updateRequirementPages_3);
     tcase_add_test(tc_core, test_updateRequirementPages_4);
+    tcase_add_test(tc_core, test_updateRequirementPages_5);
     suite_add_tcase(s, tc_core);
 
     return s;
